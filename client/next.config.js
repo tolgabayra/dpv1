f63c94a851da60c22d1efa6f2a94027b5d8a4a43
Tolga BAYRAK <bayraktolga28gmail.com>
2023-03-30 21:12:24 +0300

    newui for client

--- client/next.config.js
+++ client/next.config.js
@@ -1,3 +1,10 @@
-module.exports = {
-  reactStrictMode: true
-};
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  basePath: process.env.NODE_ENV === "production" ? "/client" : "",
+  publicRuntimeConfig: {
+    contextPath: process.env.NODE_ENV === "production" ? "/client" : "", 
+  }
+}
+
+module.exports = nextConfig
