489c47a4ffa849fdf16d13339f5173895964b58e
Tolga BAYRAK <bayraktolga28gmail.com>
2023-04-01 00:39:06 +0300

    logout and

--- server/controller/auth_controller.py
+++ server/controller/auth_controller.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, jsonify, request
 from service.auth_service import AuthService
 from util.helper import Helper
-
+import jwt
 auth_controller = Blueprint("auth_controller", __name__)
 
 
@@ -33,3 +33,30 @@ def register():
     user = AuthService.register(username, email, password)
     return jsonify({'message': 'User registered successfully', 'user': user.to_dict()}), 201
 
+
+@auth_controller.route("/logout", methods=["POST"])
+def logout():
+    response = jsonify({"message": "User logged out successfully"})
+    response.delete_cookie('access_token')
+    response.delete_cookie("refresh_token")
+    return response, 200
+
+
+
+@auth_controller.route("/verify_token", methods=["POST"])
+def verify_token():
+    access_token = request.cookies.get('access_token')
+    if access_token is None:
+        return jsonify({"error": "Missing access token"}), 401
+    try:
+        print("---------------------1------------------------")
+        decoded_token = jwt.decode(access_token, "secret_key", algorithms="HS256")
+        print("---------------------2------------------------")
+        print(decoded_token)
+        return jsonify({"user_id": decoded_token.get("some")}), 200
+    except jwt.exceptions.ExpiredSignatureError:
+        return jsonify({"error": "Token has expired"}), 401
+    except jwt.exceptions.InvalidSignatureError:
+        return jsonify({"error": "Token signature is invalid"}), 401
+    except jwt.exceptions.DecodeError:
+        return jsonify({"error": "Token decoding failed"}), 401
